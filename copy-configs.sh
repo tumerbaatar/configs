8e0f90a4ad9a1b134952d66d3e9ce52795655669
Oleg Shavrov <lambdacore2@gmail.com>
2020-11-13 23:29:46 +0300

    Add vim configs

--- copy-configs.sh
+++ copy-configs.sh
@@ -1,7 +1,9 @@
-rm -r ansible
-rm -r .config
 \cp -rf ~/ansible .
 mkdir .config
 \cp -r ~/.config/i3 .config
 \cp -r ~/.config/redshift.conf .config
 \cp -r ~/.bashrc .
+cp -r ~/.vim .
+cp -r ~/.vim_runtime .
+cp ~/.vimrc .
+cp ~/.ideavimrc .
